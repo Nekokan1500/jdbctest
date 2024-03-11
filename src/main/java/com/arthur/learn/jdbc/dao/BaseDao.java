113557b08cb60116385712a7e007a8fec688e597
arthur <zhiyuan@gmail.com>
2024-03-10 22:09:12 -0700

    Mar 10 commit

--- src/main/java/com/arthur/learn/jdbc/dao/BaseDao.java
+++ src/main/java/com/arthur/learn/jdbc/dao/BaseDao.java
@@ -1,15 +1,20 @@
 package com.arthur.learn.jdbc.dao;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
-import java.sql.JDBCType;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.arthur.learn.jdbc.utils.JDBCUtils;
 
 public class BaseDao {
 
-    public static void executeUpdate(String sql, Object ... parameters){
+    public static int executeUpdate(String sql, Object ... parameters){
         Connection connection = JDBCUtils.getConnection();
         PreparedStatement ps = null;
         try{
@@ -17,12 +22,98 @@ public class BaseDao {
             for (int i = 0; i < parameters.length; i++) {
                 ps.setObject(i+1, parameters[i]);
             }
-            ps.execute();
+            //ps.execute();
+            return ps.executeUpdate();
         } catch(SQLException e){
             e.printStackTrace();
         } finally{
             JDBCUtils.closeResource(connection, ps);
         }
+        return 0;
+    }
+
+    public static <T> T executeQuery(String sql, Class<T> clazz, Object ... parameters){
+        Connection connection = JDBCUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            connection = JDBCUtils.getConnection();
+            ps = connection.prepareStatement(sql);
+            for (int i = 0; i < parameters.length; i++) {
+                ps.setObject(i+1, parameters[i]);
+            }
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            if (rs.next()){
+                T t = clazz.getDeclaredConstructor().newInstance();
+                for (int i = 0; i < columnCount; i++) {
+                    Object value = rs.getObject(i+1);
+                    String columnLabel = rsmd.getColumnLabel(i+1);
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, value);
+                }
+                return t;
+            }
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException
+                | SQLException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } finally{
+            JDBCUtils.closeResource(connection, ps, rs);
+        }
+
+        return null;
+    }
+
+    public static <T> List<T> executeQueryForList(String sql, Class<T> clazz, Object ... parameters){
+        Connection connection = JDBCUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            connection = JDBCUtils.getConnection();
+            ps = connection.prepareStatement(sql);
+            for (int i = 0; i < parameters.length; i++) {
+                ps.setObject(i+1, parameters[i]);
+            }
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            List<T> beans = new ArrayList<T>();
+            while (rs.next()){
+                T t = clazz.getDeclaredConstructor().newInstance();
+                for (int i = 0; i < columnCount; i++) {
+                    Object value = rs.getObject(i+1);
+                    String columnLabel = rsmd.getColumnLabel(i+1);
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, value);
+                }
+                beans.add(t);
+            }
+            return beans;
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException
+                | SQLException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } finally{
+            JDBCUtils.closeResource(connection, ps, rs);
+        }
+
+        return null;
     }
     
 }
--- src/test/java/com/arthur/learn/AppTest.java
+++ src/test/java/com/arthur/learn/AppTest.java
@@ -5,9 +5,14 @@ import static org.junit.Assert.assertTrue;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.junit.Test;
+import org.junit.runner.notification.StoppedByUserException;
 
+import com.arthur.learn.jdbc.bean.Customer;
+import com.arthur.learn.jdbc.bean.Order;
 import com.arthur.learn.jdbc.dao.BaseDao;
 
 /**
@@ -49,5 +54,48 @@ public class AppTest
 
         String sql = "DELETE FROM customer WHERE name = ?";
         BaseDao.executeUpdate(sql, "Wang Feng");
+    }
+
+    @Test
+    public void testGenericQuery(){
+        String sql1 = "SELECT id, name, email FROM customer WHERE id = ?";
+        Customer customer = BaseDao.executeQuery(sql1, Customer.class, 2);
+        System.out.println("Customer: " + customer);
+        String sql2 = "SELECT * from t_order WHERE order_id = ?";
+        Order order = BaseDao.executeQuery(sql2, Order.class, 1);
+        System.out.println("Order: " + order);
+    }
+
+    @Test
+    public void testGenericQueryForList(){
+        String sql = "SELECT id, name, email FROM customer";
+        ArrayList<Customer> customers = (ArrayList<Customer>)BaseDao.executeQueryForList(sql, Customer.class);
+        /* 
+        for (Customer cust: customers){
+            System.out.println("Customer: " + cust);
+        }
+        */
+        customers.forEach(System.out::println);
+    }
+
+    // Exercise 1
+    @Test
+    public void testInsertCustomer(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter customer name: ");
+        String name = scanner.next();
+        System.out.print("Enter email: ");
+        String email = scanner.next();
+        System.out.print("Enter date of birth: ");
+        String dob = scanner.next();
+
+        String sql = "INSERT INTO customer (name, email, dob) VALUES (?, ?, ?)";
+        int ret = BaseDao.executeUpdate(sql, name, email, dob);
+        if (ret > 0){
+            System.out.println("Insert succeeded.");
+        }
+        else
+            System.out.println("Insert failed");
+        scanner.close();
     }
 }
